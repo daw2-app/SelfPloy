06fda05d60ae881cf42722f00c34a1881f490c04
Dani M <danmgiel@gmail.com>
2019-04-29 18:03:58 +0100

    Tabs hidden fixed

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -117,6 +117,7 @@ export class ChatPage implements AfterViewChecked {
     this.messagesApi.getChat(this.anotherUserId);
   }
 
+
   ionViewWillEnter() {
     if (this.navCtrl.getPrevious().name != ChatListPage.name) {
       let tabs = document.querySelectorAll('.tabbar');
@@ -130,16 +131,28 @@ export class ChatPage implements AfterViewChecked {
   }
 
 
-  ionViewDidLeave() {
-    if (this.navCtrl.getPrevious().name != ChatListPage.name) {
-      let tabs = document.querySelectorAll('.tabbar');
-      if (tabs !== null) {
-        Object.keys(tabs).map((key) => {
-          tabs[key].style.display = 'flex';
-          tabs[key].style.transform = 'translateY(0)';
-        });
-      }
+  ionViewWillLeave() {
+    let tabs = document.querySelectorAll('.show-tabbar');
+    if (tabs !== null) {
+      Object.keys(tabs).map((key) => {
+        tabs[key].style.display = 'flex';
+        tabs[key].style.transform = 'translateX(0px)';
+      });
+
     }
+  }
+
+
+  ionViewDidLeave() {
+    // if (this.navCtrl.getPrevious().name != ChatListPage.name) {
+    //   let tabs = document.querySelectorAll('.tabbar');
+    //   if (tabs !== null) {
+    //     Object.keys(tabs).map((key) => {
+    //       tabs[key].style.display = 'flex';
+    //       tabs[key].style.transform = 'translateY(0)';
+    //     });
+    //   }
+    // }
 
 
     this.sendWritingStatus(false);
